b3fc041d574bc275d4a8f35d1cc10b9113132d90
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-04-04 14:32:12 +0200

    Created Repositories for passenger, airplane, flight and booking

---
+++ src/main/java/ch/fankhauser/levin/flightbookingsystem/airplane/AirplaneRepository.java
@@ -0,0 +1,9 @@
+package ch.fankhauser.levin.flightbookingsystem.airplane;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirplaneRepository extends JpaRepository<Airplane, Long>{
+
+}
---
+++ src/main/java/ch/fankhauser/levin/flightbookingsystem/flight/FlightRepository.java
@@ -0,0 +1,9 @@
+package ch.fankhauser.levin.flightbookingsystem.flight;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+}
