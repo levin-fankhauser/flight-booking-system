472d6495fbe245a69088149a3ecdf9fa6d2c5aef
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-04-04 16:00:58 +0200

    Added swagger to simply test endpoints

---
+++ src/main/java/ch/fankhauser/levin/flightbookingsystem/swagger/SwaggerController.java
@@ -0,0 +1,12 @@
+package ch.fankhauser.levin.flightbookingsystem.swagger;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SwaggerController {
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/swagger-ui.html";
+    }
+}
