149bc244141d82fde17272936fdd6c91bce239c9
levin-fankhauser <levin.fankhauser@gmx.ch>
2025-04-11 11:29:43 +0200

    Added exception handler for specific validation errors

--- src/main/java/ch/fankhauser/levin/flightbookingsystem/base/GlobalExceptionHandler.java
+++ src/main/java/ch/fankhauser/levin/flightbookingsystem/base/GlobalExceptionHandler.java
@@ -1,14 +1,42 @@
 package ch.fankhauser.levin.flightbookingsystem.base;
 
-import org.springframework.dao.DataIntegrityViolationException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+		StringBuilder errorMessage = new StringBuilder("Validation failed for the following fields: ");
+
+		for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+			errorMessage.append(error.getField()).append(": ").append(error.getDefaultMessage()).append("; ");
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.BAD_REQUEST)
+				.body(errorMessage.toString());
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<Map<String, String>> handleConstraintViolation(ConstraintViolationException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getConstraintViolations().forEach(violation -> {
+			errors.put(violation.getPropertyPath().toString(), violation.getMessage());
+		});
+		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
 
 	@ExceptionHandler({EntityNotFoundException.class})
 	public ResponseEntity<Object> handleEntityNotFound(EntityNotFoundException exception) {
